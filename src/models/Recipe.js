d99240c923c0fc61b0e99fa1513d54a800497324
P N <petyanikleva@yahoo.com>
2021-12-08 21:52:02 +0200

    Add likes and recipe requirements in recipe model

--- src/models/Recipe.js
+++ src/models/Recipe.js
@@ -1,11 +1,27 @@
 const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
-    name: String,
-    type: String,
-    img: String,
-    difficulty: String,
-    description: String,
+    name: {
+        type: String,
+        requered: true
+    },
+    type: {
+        type: String,
+        requered: true
+    },
+    img: {
+        type: String,
+        requered: true
+    },
+    difficulty: {
+        type: String,
+        requered: true
+    },
+    description: {
+        type: String,
+        requered: true
+    },
+    likes: [],
     _ownerId: {
         type: mongoose.Types.ObjectId,
         ref: 'User'
