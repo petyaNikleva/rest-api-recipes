0f11398395f57e6128ec214049520553914b275c
P N <petyanikleva@yahoo.com>
2021-12-22 23:26:53 +0200

    make it better

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 exports.PORT = process.env.PORT || 5000;
-exports.DB_CONNECTION_STRING = process.env.MONGODB_URI || 'mongodb://localhost:27017/recipes-bul';
+//exports.DB_CONNECTION_STRING = process.env.MONGODB_URI || 'mongodb://localhost:27017/recipes-bul';
+exports.DB_CONNECTION_STRING = process.env.MONGODB_URI || 'mongodb+srv://petya:asdasd@newcluster.i0kyn.mongodb.net/db01';
 exports.JWT_SECRET = 'dodjihfDiugvkri84hr5kidf053%^6sd';
 exports.AUTH_COOKIE_NAME = 'token';
