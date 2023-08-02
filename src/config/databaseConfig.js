a90d183ca93ec3e76b2a75e1b1613d87019388d2
P N <petyanikleva@yahoo.com>
2023-08-02 13:42:36 +0300

    add .to  gitignore

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
+
 const {DB_CONNECTION_STRING} = require('../constants');
 
 exports.initDatbase = function () {
+    console.log(DB_CONNECTION_STRING)
     return mongoose.connect(DB_CONNECTION_STRING)
 };
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 exports.PORT = process.env.PORT || 5000;
 exports.DB_CONNECTION_STRING = process.env.MONGODB_URI || 'mongodb://localhost:27017/recipes-bul';
 exports.JWT_SECRET = 'dodjihfDiugvkri84hr5kidf053%^6sd';
